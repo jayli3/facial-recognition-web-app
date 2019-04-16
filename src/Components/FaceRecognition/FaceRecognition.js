bc7457e67e96351332732d583695204dd0400d8d
Jay Li <jay.li23697@gmail.com>
2019-04-17 02:22:45 +0800

    fix image margin

--- src/Components/FaceRecognition/FaceRecognition.js
+++ src/Components/FaceRecognition/FaceRecognition.js
@@ -5,7 +5,7 @@ const FaceRecognition = (props) => {
 	const {image_url, box} = props;
 	return(
 		<div className='ph3 center mb3 tc'>
-			<div className='absolute'>
+			<div className='absolute mr3'>
 				<img id='inputImage' className='br1 mb4 shadow-3' src={image_url} width='600px' height='auto' alt=''/>
 				<div className='boundingBox mb4' style={{top: box.top_row, right: box.right_col, bottom: box.bottom_row, left: box.left_col}}></div>
 			</div>
