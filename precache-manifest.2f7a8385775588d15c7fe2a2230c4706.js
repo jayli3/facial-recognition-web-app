55e8da72f8c68fcc6755b68e1b30b2cecab29e4b
Jay Li <jay.li23697@gmail.com>
2019-04-19 01:05:44 +0800

    Updates

---
+++ precache-manifest.2f7a8385775588d15c7fe2a2230c4706.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "83afed636b5db3ae4ad7d745623d4f70",
+    "url": "/facial-recognition-web-app/static/media/logo.83afed63.svg"
+  },
+  {
+    "revision": "150840d023d6e073561b",
+    "url": "/facial-recognition-web-app/static/js/runtime~main.9a1e1aaf.js"
+  },
+  {
+    "revision": "9b7d537e27bfc9a0ded7",
+    "url": "/facial-recognition-web-app/static/js/main.fae839e6.chunk.js"
+  },
+  {
+    "revision": "7decf79207d20b1ed432",
+    "url": "/facial-recognition-web-app/static/js/2.2818e383.chunk.js"
+  },
+  {
+    "revision": "9b7d537e27bfc9a0ded7",
+    "url": "/facial-recognition-web-app/static/css/main.a535ef05.chunk.css"
+  },
+  {
+    "revision": "7decf79207d20b1ed432",
+    "url": "/facial-recognition-web-app/static/css/2.962d0d41.chunk.css"
+  },
+  {
+    "revision": "f900975bb36de6a536b5592aab01a634",
+    "url": "/facial-recognition-web-app/index.html"
+  }
+];
